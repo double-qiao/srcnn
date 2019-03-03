011e8719a31c12c7fdfdc32722641bfe72d1d3a7
wangsiwei <wangsiwei@c104208.wlan.net.ed.ac.uk>
2019-03-03 22:40:24 +0000

    change

--- SRCNN/evaluate.py
+++ SRCNN/evaluate.py
@@ -36,9 +36,9 @@ def calculate_ssim(img1, img2):
     '''
     if not img1.shape == img2.shape:
         raise ValueError('Input images must have the same dimensions.')
-    if img1.shape.ndim == 2:
+    if len(img1.shape) == 2:
         return ssim(img1, img2)
-    elif img1.shape.ndim == 3:
+    elif len(img1.shape) == 3:
         if img1.shape[2] == 3:
             ssims = []
             for i in range(3):
