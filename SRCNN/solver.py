fbf6ab1c8269c684464b34fd239c228efc9ce929
wangsiwei <wangsiwei@c097200.wlan.net.ed.ac.uk>
2019-03-04 21:26:19 +0000

    change

--- SRCNN/solver.py
+++ SRCNN/solver.py
@@ -51,7 +51,7 @@ class SRCNNTrainer(object):
         torch.save(self.model, model_out_path)
         print("Checkpoint saved to {}".format(model_out_path))
 
-    def tensor_to_np(tensor):
+    def tensor_to_np(self, tensor):
         img = tensor.mul(255).byte()
         img = img.cpu().numpy().squeeze(0).transpose((1, 2, 0))
         return img
