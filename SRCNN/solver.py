8ec84924ae990b62c756d0abb2882ebe61fa0021
wangsiwei <wangsiwei@wangsiweideMacBook-Air.local>
2019-03-05 15:32:26 +0000

    change

--- SRCNN/solver.py
+++ SRCNN/solver.py
@@ -6,7 +6,7 @@ import torch
 import torch.backends.cudnn as cudnn
 import numpy as np
 from SRCNN.model import Net
-from progressbar import *
+from progress_bar import progress_bar
 from SRCNN.evaluate import calculate_ssim, calculate_psnr
 from PIL import Image
 
@@ -86,7 +86,7 @@ class SRCNNTrainer(object):
             train_loss += loss.item()
             loss.backward()
             self.optimizer.step()
-            ProgressBar(batch_num, len(self.training_loader), 'Loss: %.4f' % (train_loss / (batch_num + 1)))
+            progress_bar(batch_num, len(self.training_loader), 'Loss: %.4f' % (train_loss / (batch_num + 1)))
 
         print("    Average Loss: {:.4f}".format(train_loss / (len(self.training_loader) / self.train_batchsize)))
 
@@ -137,7 +137,7 @@ class SRCNNTrainer(object):
                 psnr = calculate_psnr(prediction, target)
                 avg_psnr += psnr
                 avg_ssim += ssim
-                ProgressBar(batch_num, len(self.testing_loader), 'PSNR: %.4f' % (avg_psnr / (batch_num + 1)), 'SSIM: %.4f' % (avg_ssim / (batch_num + 1)))
+                progress_bar(batch_num, len(self.testing_loader), 'PSNR: %.4f' % (avg_psnr / (batch_num + 1)), 'SSIM: %.4f' % (avg_ssim / (batch_num + 1)))
 
         print(" ----Average PSNR/SSIM results for ----\n\tPSNR: {:.4f} dB; SSIM: {:.4f}\n".format(avg_psnr / (len(self.testing_loader)/self.test_batchsize), avg_ssim / (len(self.testing_loader)/self.test_batchsize)))
 
