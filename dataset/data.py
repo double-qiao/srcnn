f006ab125b43acffefedb6dd0b6add394dabdb4d
wangsiwei <wangsiwei@wangsiweideMacBook-Air.local>
2019-03-05 15:23:46 +0000

    change

--- dataset/data.py
+++ dataset/data.py
@@ -4,37 +4,58 @@ from .dataset import MyDataset_train, MyDataset_test
 def get_data_set():
     return './dataset'
 
-def input_transform():
+def input_transform_test():
 
     return transforms.Compose(
         [
+            Resize(crop_size // upscale_factor, interpolation=Image.BICUBIC),
             transforms.ToTensor()
 
         ]
     )
 
-def target_transform():
+def target_transform_test():
 
     return transforms.Compose(
         [
             transforms.ToTensor()
         ]
     )
+
+
+def input_transform_train(crop_size, upscale_factor):
+    return Compose([
+        CenterCrop(crop_size),
+        Resize(crop_size // upscale_factor, interpolation=Image.BICUBIC),
+        ToTensor(),
+    ])
+
+
+def target_transform_train(crop_size):
+    return Compose([
+        CenterCrop(crop_size),
+        ToTensor(),
+    ])
+
 
 def get_training_set():
     # root_dir = get_data_set()
     # train_dir = join(root_dir, "train")
     train_dir = "/home/s1825980/datasets/DIV2K"
     # train_dir = "/home/s1825980/datasets/valid_div2k"
+    crop_size = 648
+    upscale_factor = 4
+
     return MyDataset_train(train_dir,
-                             input_transform = input_transform(),
-                             target_transform = target_transform())
+                             input_transform = input_transform_train(crop_size, upscale_factor),
+                             target_transform = target_transform_train(crop_size))
 
 
 def get_test_set():
     # root_dir = get_data_set()
     # test_dir = join(root_dir, "test")
     test_dir = "/home/s1825980/datasets/valid_div2k"
+    upscale_factor = 4
     return MyDataset_test(test_dir,
-                             input_transform=input_transform(),
-                             target_transform=target_transform())
+                             input_transform=input_transform_test(upscale_factor),
+                             target_transform=target_transform_test())
--- dataset/dataset.py
+++ dataset/dataset.py
@@ -2,7 +2,6 @@ from os import listdir
 from os.path import join
 
 import torch.utils.data as data
-import numpy as np
 from torchvision import transforms
 from PIL import Image
 
@@ -14,27 +13,6 @@ def is_image_file(filename):
 def load_img(filepath):
     img = Image.open(filepath)
     return img
-
-def resize_img(img):
-    img = np.array(img)
-    print(img)
-    n = 4
-    if img.shape[1] % n == 0 and img.shape[0] % n == 0:
-        pass
-    elif img.shape[1] % n == 0:
-        img = img[0:(img.shape[0] - (img.shape[0] % n)), :, :]
-        img = Image.fromarray(img, mode='RGB')
-    elif img.shape[0] % n == 0:
-        img = img[:, 0:(img.shape[1] - (img.shape[1] % n)), :]
-        img = Image.fromarray(img, mode='RGB')
-    else:
-        img = img[0:(img.shape[0] - (img.shape[0] % n)), 0:(img.shape[1] - (img.shape[1] % n)), :]
-        img = Image.fromarray(img, mode='RGB')
-    # arr_img = np.asarray(img)
-    # print(img.shape[1])
-    # img_resize = cv2.resize(img, (int(img.shape[1]/n), int(img.shape[0]/n)), interpolation=cv2.INTER_CUBIC)
-    img_resize = img.resize(int(img.size / n), Image.BICUBIC)
-    return img_resize
 
 # def tensor_to_PIL(tensor):
 #     image = tensor.cpu().clone()
@@ -55,18 +33,19 @@ class MyDataset_train(data.Dataset):
         #     join(image_dir + "/DIV2K_valid_HR(srgan for X16)/DIV2K_valid_HR_ground_truth(srgan for X16)", x) for x in
         #     listdir(image_dir + "/DIV2K_valid_HR(srgan for X16)/DIV2K_valid_HR_ground_truth(srgan for X16)") if
         #     is_image_file(x)]
+
         self.input_transform = input_transform
         self.target_transform = target_transform
 
     def __getitem__(self, index):
-        target = load_img(self.image_filenames[index])
-        input_image = resize_img(target)
+        input = load_img(self.image_filenames[index])
+        target = input
         if self.input_transform:
-            input_image = self.input_transform(input_image)
+            input = self.input_transform(input)
         if self.target_transform:
             target = self.target_transform(target)
 
-        return input_image, target
+        return input, target
 
     def __len__(self):
         return len(self.image_filenames)
@@ -82,14 +61,14 @@ class MyDataset_test(data.Dataset):
         self.target_transform = target_transform
 
     def __getitem__(self, index):
-        target = load_img(self.image_filenames[index])
-        input_image = resize_img(target)
+        input = load_img(self.image_filenames[index])
+        target = input.copy()
         if self.input_transform:
-            input_image = self.input_transform(input_image)
+            input = self.input_transform(input)
         if self.target_transform:
             target = self.target_transform(target)
 
-        return input_image, target
+        return input, target
 
     def __len__(self):
         return len(self.image_filenames)
