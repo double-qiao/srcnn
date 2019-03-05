e0906c17ffa6ed697ed4596409d7b8ce939b0f80
wangsiwei <wangsiwei@wangsiweideMacBook-Air.local>
2019-03-05 09:31:18 +0000

    change

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ from dataset.data import get_training_set, get_test_set
 # ===========================================================
 parser = argparse.ArgumentParser(description='PyTorch Super resolution task')
 # hyper-parameters
-parser.add_argument('--batchSize', type=int, default=1, help='training batch size')
+parser.add_argument('--batchSize', type=int, default=10, help='training batch size')
 parser.add_argument('--testBatchSize', type=int, default=1, help='testing batch size')
-parser.add_argument('--nEpochs', type=int, default=50, help='number of epochs to train for')
+parser.add_argument('--nEpochs', type=int, default=1, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.01, help='Learning Rate. Default=0.01')
 parser.add_argument('--seed', type=int, default=123, help='random seed to use. Default=123')
 
